b45555d6a67b2bed4ccb984a5a3fb7959622db51
Briand Steve <steve.briand7@gmail.com>
2025-02-11 09:14:12 +0000

    com: avoir la fonction git de paramétrage

--- 1_chargement_donnees.R
+++ 1_chargement_donnees.R
@@ -7,7 +7,19 @@ summary(iris)
 
 iris <- iris %>% mutate(trop_cool = 1) %>% filter(trop_cool == 1)
 
+usethis::use_git_config(
+  scope = "user",
+  # Définition du nouveau caractère de fin de ligne par défaut:
+  core.autocrlf = "input",
+  # Sur Linux ou MacOS
+  ## core.autocrlf = "true", # Sur Windows
+  # Définition de l'editeur git nano à la place de vim
+  core.editor = "nano",
+  pull.rebase = "false"
+)
+
 # git commit --allow-empty -m "Résolution de conflit (commit vide)"
+
 
 
 
